11eedf1c97772b83eb4d3e286fbc567e4f33053b
AnkitSomkuwar <146408398+AnkitSomkuwar@users.noreply.github.com>
2023-12-26 16:14:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -47,16 +47,68 @@ app.get("/movies/", async (request, response) => {
   );
 });
 
-app.post("/movies/", async (request, response) => {
-  const { directorId, movieName, leadActor } = request.body;
+app.post('/movies/', async (request, response) => {
+  const {directorId, movieName, leadActor} = request.body
   const postMovieQuery = `
-  
   INSERT INTO
     movie ( director_id, movie_name, lead_actor)
   VALUES
-    (${directorId}, '${movieName}', '${leadActor}');`;
-  await db.run(postMovieQuery);
-  response.send("Movie Successfully Added");
-});
+    (${directorId}, '${movieName}', '${leadActor}');`
+  await db.run(postMovieQuery)
+  response.send('Movie Successfully Added')
+})
 
-module.exports = app;
+app.put('/movies/:movieId/', async (request, response) => {
+  const {movieId} = request.params
+  const movieDetails = request.body
+  const {directorId, movieName, leadActor} = movieDetails
+  const updateMovieQuery = `
+  UPDATE MOVIE 
+  SET 
+  director_id = '${directorId}',
+  movie_name = '${movieName}',
+  lead_actor = '${leadActor}'
+  WHERE
+  movie_id = ${movieId};`
+  await db.run(updateMovieQuery)
+  response.send('Movie Details Updated')
+})
+
+app.delete('/movies/:movieId/', async (request, response) => {
+  const {movieId} = request.params
+  const deleteMovieQuery = `
+  DELETE FROM
+   MOVIE
+   WHERE 
+movie_id = ${movieId};`
+  await db.run(deleteMovieQuery)
+  response.send('Movie Removed')
+})
+
+app.get('/directors/', async (request, response) => {
+  const gettingMovie = `
+  SELECT *
+  FROM director
+`
+  const movieArray = await db.all(gettingMovie)
+  response.send(
+    movieArray.map(eachMovie => convertDbObjectToResponseObject(eachMovie)),
+  )
+})
+
+app.get('/directors/:directorId/movies/', async (request, response) => {
+  const {directorId} = request.params
+  const getDirectorMovie = `
+  SELECT movie_name
+  FROM MOVIE
+  WHERE director_id = ${directorId} ;
+  `
+  const directorNames = await db.all(getDirectorMovie)
+  response.send(
+    directorNames.map(eachDirector =>
+      convertDbObjectToResponseObject(eachDirector),
+    ),
+  )
+})
+
+module.exports = app
